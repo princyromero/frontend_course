18b6c9054226938be50032746fd7cfb3906e8499
arokia romero princy <arokiaromeroprincy@gmail.com>
2026-09-24 01:24:47 +0530

    update

---
+++ JS/Day3/index.js
@@ -0,0 +1,155 @@
+let a = "x"
+a += "y"
+console.log(a);
+
+let name ="harini"
+name += "sri"
+console.log(name);
+
+let course = "full"
+course += "stack"
+console.log(course);
+
+let city = "chennai"
+city += "ashkoknagar"
+console.log(city);
+
+let color = "pink"
+color += "purple"
+console.log(color);
+
+let website = "amazon"
+website += "flipkart"
+console.log(website);
+
+let frontend ="html"
+frontend += "css"
+console.log(frontend);
+
+let backend ="node"
+backend += "js"
+console.log(backend);
+
+let browser = "google"
+browser += "chrome"
+console.log(browser);
+
+let language = "english"
+language += "tamil"
+console.log(language);
+
+let application = "web"
+application += "site"
+console.log(application);
+
+let programming = "java"
+programming += "Script"
+console.log(programming);
+
+let database = "analysis"
+database += "tools"
+console.log(database);
+
+let component ="table"
+component += "form"
+console.log(component);
+
+let mobile = "samsung"
+mobile += "moto"
+console.log(mobile);
+
+let company ="Infosys"
+company += "asian"
+console.log(company);
+
+let structure = "types"
+structure += "tags"
+console.log(structure);
+
+let design = "create"
+design += "style"
+console.log(design);
+
+let season = "summer"
+season += "spring"
+console.log(season);
+
+let brand = "nike"
+brand += "Adidas"
+console.log(brand);
+
+let months = "july"
+months += "june"
+console.log(months);
+
+
+
+let count = 13;
+count = 20;
+console.log(count); 
+
+let firstnumber = 55;
+firstnumber += 15;
+console.log(firstnumber);  
+
+let height = 25;
+height += 15;
+console.log(height);
+
+let weight = 40;
+weight += 55;
+console.log(weight);
+
+let widht = 21;
+widht += 4;
+console.log(widht);
+
+let number1 = 20;
+number1 += 10;
+console.log(number1);
+
+let problem = 5;
+problem += 5;
+console.log(problem);
+
+let calculation = 25;
+calculation += 5;
+console.log(calculation);
+
+let padding = 42;
+padding += 21;
+console.log(padding);
+
+let rollnumber = 13;
+rollnumber += 24;
+console.log(rollnumber);
+
+let salary = 10000;
+salary += 20000;
+console.log(salary);
+
+let mark = 65;
+mark += 55;
+console.log(mark);
+
+let score = 150;
+score += 120;
+console.log(score);
+
+let age = 13;
+age += 20;
+console.log(age);
+
+let percentage = 75;
+percentage += 55;
+console.log(percentage);
+
+
+
+
+
+
+
+
+
+
---
+++ JS/Practice/index.js
@@ -0,0 +1,8 @@
+let x=20;
+let y=10;
+console.log(x+y)
+
+let firstnumber =13;
+let secnumber=20;
+let total=firstnumber+secnumber;
+console.log(total) 
