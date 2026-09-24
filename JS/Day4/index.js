bcb5b882712cffc72efc69d0c720cc10c289cfe0
arokia romero princy <arokiaromeroprincy@gmail.com>
2026-09-25 00:55:08 +0530

    update

---
+++ JS/Day4/index.js
@@ -0,0 +1,82 @@
+let number = 10;
+
+if (number > 0) {
+    console.log("Positive Number");
+}
+
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("Eligible to Vote");
+} else {
+    console.log("Not Eligible to Vote");
+}
+
+
+
+
+
+for(let a=1; a<=50; a++){
+    console.log(a);
+}
+
+for(let a=1;a<=50; a++){
+    if(a<=25){
+        console.log(a);
+    }
+}
+
+for(let x=10; x>=1; x--){
+    console.log(x);
+}
+
+for(let x=50; x>=1; x--){
+    if(x >=25){
+        console.log(x);
+    }
+}
+
+for (let a=1; a<=5; a++){
+    if(Number <=5){
+        console.log(number);
+    }
+}
+
+let score =35;
+if(score >18){
+    console.log("score is greater than 18");
+}
+
+let value =5;
+if(value !==10){
+    console.log("value is not 10");
+}
+
+let city ="chennai"
+if(city != "bangalore"){
+    console.log("city is not bangalore");
+}
+
+let mark =50;
+if(mark ===50){
+    console.log("score is exactly 50");
+}
+
+let name ="raja"
+if(name == "raja"){
+    console.log("name is raja");
+} 
+
+for (let price =100; price <=500; price +=100){
+    if (price <=300){
+        console.log(price);
+    }
+}
+
+let userproduct ="milk";
+for(let count =1; count <=3; count++){
+    if(userproduct === "milk"){
+        console.log(userproduct);
+    }
+}
---
+++ JS/Practice2/index.js
@@ -0,0 +1,20 @@
+// let money =100;
+// let milk =30;
+// let biscuit =10;
+// let chocolate =20;
+// let icecream =40;
+
+// if (money>=30){
+//     console.log("milk");
+// }
+
+// if(money>=10){
+//     console.log("biscuit");
+// }
+
+
+for (let x=1 ; x<=100; x++) {
+    console.log(x);
+}
+
+  
